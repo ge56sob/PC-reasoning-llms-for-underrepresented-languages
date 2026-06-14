98859a2e5e9132d0cba1f3bcd8e02cb76b8e2c25
ge56sob <ge56sob@tum.de>
2026-06-14 06:57:18 +0200

    Update Cuong-script.sh

--- Cuong/Cuong-script.sh
+++ Cuong/Cuong-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition lrz-hgx-h100-94x4
 #SBATCH --gres gpu:1
-#SBATCH --time 2-00:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output %j.out
 
 python cuong_small_model_3.py
