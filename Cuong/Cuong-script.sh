b5fc300328847ab80192d5b607c01ec0a44dc819
ge56sob <ge56sob@tum.de>
2026-06-15 17:10:17 +0200

    Update Cuong-script.sh

--- Cuong/Cuong-script.sh
+++ Cuong/Cuong-script.sh
@@ -4,6 +4,11 @@
 #SBATCH --time=12:00:00
 #SBATCH --output %j.out
 
+python -m pip install --user huggingface_hub
+python -m pip install --user datasets
+python -m pip install --user transformers==4.52.4
+python -m pip install --user accelerate sentencepiece
+
 python cuong_small_model_3.py
 
 git add -A && \
