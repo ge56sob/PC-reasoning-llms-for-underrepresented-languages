7d39ca24d91d5ee475958ce2bb7f98303240b3e9
ge56sob <ge56sob@tum.de>
2026-06-19 03:38:54 +0200

    Update Cuong-script_v2.sh

--- Cuong/Cuong-script_v2.sh
+++ Cuong/Cuong-script_v2.sh
@@ -4,13 +4,11 @@
 #SBATCH --time=12:00:00
 #SBATCH --output %j.out
 
-export HF_TOKEN="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"
-python -m pip install --user huggingface_hub
 python -m pip install --user datasets
-python -m pip install --user transformers==4.52.4
+python -m pip install --user transformers
 python -m pip install --user accelerate sentencepiece
 
-python cuong_small_model_3_v2.py
+python cuong_small_model_3.py
 
 git add -A && \
 git commit -m "automated push" && \
