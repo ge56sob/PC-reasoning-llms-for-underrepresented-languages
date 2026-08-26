403bf6d876e9b40924e1c9097cfdc50bdbc7422d
ge56sob <ge56sob@tum.de>
2026-08-26 06:13:36 +0200

    Update Cuong_largemodel_script.sh

--- Cuong/Large/Cuong_largemodel_script.sh
+++ Cuong/Large/Cuong_largemodel_script.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 #SBATCH --partition lrz-hgx-h100-94x4
 #SBATCH --gres gpu:1
-#SBATCH --time=48:00:00
+export TENSOR_PARALLEL_SIZE=2
+#SBATCH --time 2-00:00:00
 #SBATCH --output %j.out
 
-python -m pip install --user datasets
-python -m pip install --user transformers
-python -m pip install --user accelerate sentencepiece
+export HF_TOKEN=
+export HUGGINGFACE_HUB_TOKEN=$HF_TOKEN
 
-export HF_HOME=/dss/dssfs05/lwp-dss-0003/pn39je/pn39je-dss-0004/go35wit2/huggingface
-export HF_HUB_CACHE=$HF_HOME/models
-export HF_DATASETS_CACHE=$HF_HOME/datasets
+export BIGDIR=/dss/dssfs05/lwp-dss-0003/pn39je/pn39je-dss-0004/go35wit2
 
-python Cuong_LargeModel.py
+export HF_HOME=$BIGDIR/huggingface
+export HF_HUB_CACHE=$BIGDIR/huggingface/hub
+export TRANSFORMERS_CACHE=$BIGDIR/huggingface/transformers
+export HF_DATASETS_CACHE=$BIGDIR/huggingface/datasets
+export XDG_CACHE_HOME=$BIGDIR/.cache
+export TMPDIR=$BIGDIR/tmp
+
+mkdir -p $HF_HOME $HF_HUB_CACHE $TRANSFORMERS_CACHE $HF_DATASETS_CACHE $XDG_CACHE_HOME $TMPDIR
+
+cd $BIGDIR 
+
+export HF_HUB_DISABLE_XET=1
+
+python3 -u Cuong_LargeModel.py
 
 git add -A && \
 git commit -m "automated push" && \
